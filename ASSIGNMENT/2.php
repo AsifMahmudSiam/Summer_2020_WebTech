34fef8081198f6726982fe58da8eaa261bcf348d
AsifMahmudSiam <62529690+AsifMahmudSiam@users.noreply.github.com>
2020-07-23 21:14:40 +0600

    new update

--- ASSIGNMENT/2.php
+++ ASSIGNMENT/2.php
@@ -17,6 +17,33 @@ if (empty($_POST['gender']))
 {
 	$errors['gender1']= "You must fill up the field";
 }
+if (isset($_POST['degree1']) || isset($_POST['degree2']) || isset($_POST['degree3'])) 
+{
+	$a=$_POST['degree1'];
+	$b=$_POST['degree2'];
+	$c=$_POST['degree3'];
+	echo $a;
+	echo $b;
+	echo $c;
+	
+}
+else
+{
+	if (isset($_POST['submit'])) {
+		# code...
+		echo "At least one of them has to be selected";
+	}
+}
+if (empty($_POST['degree']))
+{
+	$errors['degree1']= "You must fill up the field";
+}
+if (empty($_POST['blood']))
+{
+	$errors['blood1']= "You must fill up the field";
+}
+
+
 if(count($errors)==0)
 {
 	header("Location: 3.php");
@@ -34,26 +61,39 @@ if(count($errors)==0)
 	<form target="" method="POST">
 		<fieldset>
 			<legend>Personal Information</legend>
-			Name:<input type="text" name="name" value=""><br>
+			Name:<input type="text" name="name" value="<?php if(isset($_POST['name']));?>"><br>
 			<p><?php if(isset($errors['name1'])) echo $errors['name1'];?></p>
 			
-			Email:<input type="email" name="email" value=""><br>
+			Email:<input type="email" name="email" value="<?php if(isset($_POST['email']));?>"><br>
 			<p><?php if(isset($errors['email1'])) echo $errors['email1'];?></p>
 		
 
 
-			Gender:<input type="radio" name="gender">  Male 
-						<input type="radio" name="gender"> Female
-						<input type="radio" name="gender"> Other<br>
+			Gender:<input type="radio" name="gender" value="<?php if(isset($_POST['gender']));?>">  Male 
+						<input type="radio" name="gender" value="<?php if(isset($_POST['gender']));?>"> Female
+						<input type="radio" name="gender" value="<?php if(isset($_POST['gender']));?>"> Other<br>
+
 
                     <p><?php if(isset($errors['gender1'])) echo $errors['gender1'];?></p>
-						DATE:<input type="text" size="3%">/<input type="text" size="3%">/<input type="text" size="5%"> (dd/mm/yyyy)<br>
 						
+						DATE:<input type="text" size="3%">/<input type="text" size="3%">/<input type="text" size="5%"> (dd/mm/yyyy)<br>
+						<p><?php if(isset($errors['date1'])) echo $errors['date1'];?></p>
 
-						Degree:	<input type="checkbox"> SSC 
-						<input type="checkbox"> HSC 
-						<input type="checkbox"> BSc. 
-						<input type="checkbox"> MSc.<br> 
+						Degree:	<input type="checkbox" name="degree1" value="SSC"> SSC
+			<input type="checkbox" name="degree2" value="HSC"> HSC
+			<input type="checkbox" name="degree3" value="BSc"> BSc<br>
+						<p><?php if(isset($errors['degree1'])) echo $errors['degree1'];?></p>
+
+						<td>Blood Group</td>
+					<td>
+						<select>
+							<option>A+</option>
+							<option>B+</option>
+							<option>O+</option>
+							<option>AB+</option>
+						</select>
+						<p><?php if(isset($errors['blood1'])) echo $errors['degree1'];?></p>
+					</td>
 					
 					<input type="submit" value="submit"><br>	
 
@@ -65,5 +105,6 @@ if(count($errors)==0)
 
 </body>
 </html>
+
 
 
