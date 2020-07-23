45e5498dbffcf0637c14d546817b331d84b000ae
AsifMahmudSiam <62529690+AsifMahmudSiam@users.noreply.github.com>
2020-07-23 22:07:20 +0600

    final update

--- ASSIGNMENT/2.php
+++ ASSIGNMENT/2.php
@@ -30,7 +30,7 @@ if (isset($_POST['degree1']) || isset($_POST['degree2']) || isset($_POST['degree
 else
 {
 	if (isset($_POST['submit'])) {
-		# code...
+		
 		echo "At least one of them has to be selected";
 	}
 }
@@ -41,6 +41,58 @@ if (empty($_POST['degree']))
 if (empty($_POST['blood']))
 {
 	$errors['blood1']= "You must fill up the field";
+}
+$flag=true;
+
+if (isset($_GET['day'])) {
+
+	$a=(int)$_GET['day'];
+	if (strlen($a)>0) {
+		
+		if ($a>0 && $a<=31) {
+		
+			echo $a;
+		}
+		else
+		{
+			$flag=false;
+		}
+	}
+}
+
+if (isset($_GET['month'])) {
+
+	$a=(int)$_GET['month'];
+	if (strlen($a)>0) {
+	
+		if ($a>0 && $a<=12) {
+			
+			echo $a;
+		}
+		else
+		{
+			$flag=false;
+		}
+	}
+}
+if (isset($_GET['year'])) {
+	# code...
+	$a=(int)$_GET['year'];
+	if (strlen($a)>0) {
+	
+		if ($a>=1900 && $a<=2016) {
+		
+			echo $a;
+		}
+		else
+		{
+			$flag=false;
+		}
+	}
+}
+if ($flag==false) {
+	# code.
+	echo "invalid";
 }
 
 
@@ -76,13 +128,21 @@ if(count($errors)==0)
 
                     <p><?php if(isset($errors['gender1'])) echo $errors['gender1'];?></p>
 						
-						DATE:<input type="text" size="3%">/<input type="text" size="3%">/<input type="text" size="5%"> (dd/mm/yyyy)<br>
+						DATE:<pre class="tab">dd   mm     yyyy</pre><br>
+			<input type="text" name="day" size="1%">/
+			<input type="text" name="month" size="1%">/
+			<input type="text" name="year" size="2%"><br>
+			<br>
 						<p><?php if(isset($errors['date1'])) echo $errors['date1'];?></p>
 
 						Degree:	<input type="checkbox" name="degree1" value="SSC"> SSC
 			<input type="checkbox" name="degree2" value="HSC"> HSC
 			<input type="checkbox" name="degree3" value="BSc"> BSc<br>
 						<p><?php if(isset($errors['degree1'])) echo $errors['degree1'];?></p>
+						<p><?php if(isset($errors['degree2'])) echo $errors['degree2'];?></p>
+						<p><?php if(isset($errors['degree3'])) echo $errors['degree3'];?></p>
+
+
 
 						<td>Blood Group</td>
 					<td>
@@ -96,6 +156,8 @@ if(count($errors)==0)
 					</td>
 					
 					<input type="submit" value="submit"><br>	
+
+
 
 
 		</fieldset>
