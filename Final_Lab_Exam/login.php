4f84a8474b8bad62a292174f1fb3117bce579b46
AsifMahmudSiam <62529690+AsifMahmudSiam@users.noreply.github.com>
2020-09-06 08:56:36 +0600

    login update

--- Final_Lab_Exam/login.php
+++ Final_Lab_Exam/login.php
@@ -1,3 +1,31 @@
+
+
+<?php
+
+	if (isset($_GET['error'])) {
+		
+		if($_GET['error'] == 'null_value'){
+			echo "Username/Password field can't left empty...";
+		}
+
+		if($_GET['error'] == 'invalid_user'){
+			echo "Invalid username or Password";
+		}
+
+		if($_GET['error'] == 'invalid_request'){
+			echo "You have to login first...";
+		}
+
+	}else if(isset($_GET['success'])){
+		
+		if($_GET['success'] == 'registration_done'){
+			echo "Registration Done! Now you can login...";
+		}
+	}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
